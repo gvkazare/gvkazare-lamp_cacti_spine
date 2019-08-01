08cf054622fc99cd515abb6db0dd8b70ed649b37
gvkazare <47571222+gvkazare@users.noreply.github.com>
2019-08-01 14:58:51 +0300

    Update restore.sh

--- cacti_files/restore.sh
+++ cacti_files/restore.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+echo "Start restore"
+
+echo "=> MYSQL DB restore"
 gunzip < /var/backups/cacti_db.gz | mysql -uroot cacti
+
+echo "=> RRA restore"
 tar xzf /var/backups/cacti_rra.tar.gz -C /
+
+echo "End restore"
